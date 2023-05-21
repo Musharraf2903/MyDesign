c9dd6ae1e91ae1fe5324f6a64b6a312ea2a38591
Musharraf2903 <satkutmusharraf@gmail.com>
2023-05-21 23:43:29 +0530

    Initial commit

---
+++ slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#service-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 1000, // 
+      dots: false, // Hide navigation dots
+      arrows: true // Add navigation arrows
+    });
+  });
+  
